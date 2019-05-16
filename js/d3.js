384f130225a53a4fb61f4d569bb8b2fdb3200eb1
kevin60523 <kai861108@yahoo.com.tw>
2019-05-16 12:48:41 +0800

    add number on linechart

--- js/d3.js
+++ js/d3.js
@@ -62,6 +62,25 @@ function init_linechart(minrally,maxrally){
                 line: {
                     tension: 0 // disables bezier curves
                 }
+            },
+            "animation": {
+              "duration": 1,
+              "onComplete": function() {
+                var chartInstance = this.chart,
+                  ctx = chartInstance.ctx;
+
+                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'bottom';
+
+                this.data.datasets.forEach(function(dataset, i) {
+                  var meta = chartInstance.controller.getDatasetMeta(i);
+                  meta.data.forEach(function(bar, index) {
+                    var data = dataset.data[index];
+                    ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                  });
+                });
+              }
             }
         };
 
