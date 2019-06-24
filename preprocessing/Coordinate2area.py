84b7316d26cb3b9e2b08f3ad627769dc8759332a
kevin60523 <kai861108@yahoo.com.tw>
2019-06-24 20:45:08 +0800

    modify coordinate2area.py

--- preprocessing/Coordinate2area.py
+++ preprocessing/Coordinate2area.py
@@ -13,13 +13,13 @@ for i in range(len(df)):
 	if df['hit_y'][i] <= 468 and df['hit_y'][i] > 307 and df['hit_x'][i] <= 32:
 		hitarea += ["D1"]
 	if df['hit_y'][i] <= 629 and df['hit_y'][i] > 468 and df['hit_x'][i] <= 32:
-		hitarea += ["D1"]
+		hitarea += ["E1"]
 	if df['hit_y'][i] <= 871 and df['hit_y'][i] > 629 and df['hit_x'][i] <= 32:
-		hitarea += ["D2"]
+		hitarea += ["E2"]
 	if df['hit_y'][i] <= 935 and df['hit_y'][i] > 871 and df['hit_x'][i] <= 32:
-		hitarea += ["D3"]
+		hitarea += ["E3"]
 	if df['hit_y'][i] > 935 and df['hit_x'][i] <= 32:
-		hitarea += ["D4"]
+		hitarea += ["E4"]
 	if df['hit_y'][i] <= 0 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
 		hitarea += ["B4"]
 	if df['hit_y'][i] <= 74 and df['hit_y'][i] > 0 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
@@ -29,13 +29,13 @@ for i in range(len(df)):
 	if df['hit_y'][i] <= 468 and df['hit_y'][i] > 307 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
 		hitarea += ["B1"]
 	if df['hit_y'][i] <= 629 and df['hit_y'][i] > 468 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
-		hitarea += ["B1"]
+		hitarea += ["C1"]
 	if df['hit_y'][i] <= 871 and df['hit_y'][i] > 629 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
-		hitarea += ["B2"]
+		hitarea += ["C2"]
 	if df['hit_y'][i] <= 935 and df['hit_y'][i] > 871 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
-		hitarea += ["B3"]
+		hitarea += ["C3"]
 	if df['hit_y'][i] > 935 and df['hit_x'][i] > 32 and df['hit_x'][i] <= 152:
-		hitarea += ["B4"]
+		hitarea += ["C4"]
 	if df['hit_y'][i] <= 0 and df['hit_x'][i] > 152 and df['hit_x'][i] <= 272:
 		hitarea += ["A4"]
 	if df['hit_y'][i] <= 74 and df['hit_y'][i] > 0 and df['hit_x'][i] > 152 and df['hit_x'][i] <= 272:
@@ -61,13 +61,13 @@ for i in range(len(df)):
 	if df['hit_y'][i] <= 468 and df['hit_y'][i] > 307 and df['hit_x'][i] > 272 and df['hit_x'][i] <= 392:
 		hitarea += ["C1"]
 	if df['hit_y'][i] <= 629 and df['hit_y'][i] > 468 and df['hit_x'][i] > 272 and df['hit_x'][i] <= 392:
-		hitarea += ["C1"]
+		hitarea += ["B1"]
 	if df['hit_y'][i] <= 871 and df['hit_y'][i] > 629 and df['hit_x'][i] > 272 and df['hit_x'][i] <= 392:
-		hitarea += ["C2"]
+		hitarea += ["B2"]
 	if df['hit_y'][i] <= 935 and df['hit_y'][i] > 871 and df['hit_x'][i] > 272 and df['hit_x'][i] <= 392:
-		hitarea += ["C3"]
+		hitarea += ["B3"]
 	if df['hit_y'][i] > 935 and df['hit_x'][i] > 272 and df['hit_x'][i] <= 392:
-		hitarea += ["C4"]
+		hitarea += ["B4"]
 	if df['hit_y'][i] <= 0 and df['hit_x'][i] > 392:
 		hitarea += ["E4"]
 	if df['hit_y'][i] <= 74 and df['hit_y'][i] > 0 and df['hit_x'][i] > 392:
@@ -77,12 +77,12 @@ for i in range(len(df)):
 	if df['hit_y'][i] <= 468 and df['hit_y'][i] > 307 and df['hit_x'][i] > 392:
 		hitarea += ["E1"]
 	if df['hit_y'][i] <= 629 and df['hit_y'][i] > 468 and df['hit_x'][i] > 392:
-		hitarea += ["E1"]
+		hitarea += ["D1"]
 	if df['hit_y'][i] <= 871 and df['hit_y'][i] > 629 and df['hit_x'][i] > 392:
-		hitarea += ["E2"]
+		hitarea += ["D2"]
 	if df['hit_y'][i] <= 935 and df['hit_y'][i] > 871 and df['hit_x'][i] > 392:
-		hitarea += ["E3"]
+		hitarea += ["D3"]
 	if df['hit_y'][i] > 935 and df['hit_x'][i] > 392:
-		hitarea += ["E4"]
+		hitarea += ["D4"]
 df['hit_area']=hitarea
 export_excel = df.to_excel (r'../../clip_info_tai.xlsx', index = None, header=True) #Don't forget to add '.xlsx' at the end of the path
