028b6e7babce39d54ed7631890134e9749f49abc
yao0510 <sf163864@gmail.com>
2019-07-27 16:45:35 +0800

    add upload validation to avoid empty submit

--- js/autoanalysis.js
+++ js/autoanalysis.js
@@ -5,7 +5,7 @@ function checkfile(sender) {
     var fileExt = sender.value;
     fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
     if (validExts.indexOf(fileExt) < 0) {
-      alert("檔案類型錯誤，可接受的副檔名有：" + validExts.toString());
+      alert("File tpye is not acceptable,please upload correct file extension：" + validExts.toString());
       sender.value = null;
       return false;
     }
@@ -14,6 +14,10 @@ function checkfile(sender) {
 
 $(function () {
     $('#submit-video').submit(function(e) {
+        if(document.getElementById('video-uploader').value.length == 0){
+            alert("Please upload file.");
+            return false;
+        }
         e.preventDefault(); // avoid to execute the actual submit of the form.
         var data = $('#submit-video').serialize();
         // var url = $(this).attr("action");
