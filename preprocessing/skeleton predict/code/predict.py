9d804bace4360c537390e0adc6aec09d3e88a69c
yao0510 <sf163864@gmail.com>
2019-09-16 23:33:07 +0800

    modify predict passive/active by using probability and use best threshold

--- preprocessing/skeleton predict/code/predict.py
+++ preprocessing/skeleton predict/code/predict.py
@@ -5,6 +5,7 @@ from sklearn.externals import joblib
 from sklearn import svm
 from sklearn.metrics import *
 from xgboost import XGBClassifier
+from collections import Counter
 import itertools
 import matplotlib.pyplot as plt
 
@@ -79,21 +80,75 @@ def XGBoost(filename, x_predict, model_name, xgb_outputname, set_now):
 	label = [1, 2]
 
 	model = joblib.load(model_name)
-	prediction = model.predict(x_predict)
+	prediction = model.predict_proba(x_predict)
+
+	all_precision = []
+	all_recall = []
+	max_threshold = 0
+	best_pre_rec = 1e9
+	best_pre = 0
+	best_rec = 0
+	score_threshold = 0.60
+	for threshold in range(1,100) :
+		final_answer = []
+		# Replace probability to class answer, 2 = active, 1 = passive
+		for i in range(0,np.shape(prediction)[0]) :
+			if prediction[i][1] > (0.01*threshold) :
+				final_answer.append(2)
+			else :
+				final_answer.append(1)
+
+		precision = precision_score(data['hit_height'], final_answer, labels = label, average=None)[0]
+		recall = recall_score(data['hit_height'], final_answer, labels = label, average=None)[0]
+		all_precision.append(precision)
+		all_recall.append(recall)
+
+		# Select best sum of precision and recall if they both > score threshold
+		if abs(precision - recall) < best_pre_rec and precision > score_threshold and recall > score_threshold :
+			best_pre_rec = abs(precision - recall)
+			best_pre = precision
+			best_rec = recall
+			max_threshold = 0.01*threshold
+
+	print("===============================")
+	print("Best threshold = ",max_threshold)
+	print("Best precision = ",best_pre)
+	print("Best recall = ",best_rec)
+	
+
+	score_threshold_plot = [score_threshold for _ in range(100)]
+	fig=plt.figure(figsize=(12.8,7.2))
+	plt.plot(all_precision,'-',label='precision')
+	plt.plot(all_recall,'-',label='recall')
+	plt.plot(score_threshold_plot,'r-',label='score threshold')
+	plt.legend(loc='best')
+	plt.xlabel('Threshold(*0.01)')
+	plt.ylabel('Percentage')
+	fig.savefig('set' + str(set_now) + '_precision_recall.jpg')
+	plt.clf()
+
+	#Classify proba answer to class
+	final_answer = []
+	for i in range(0,np.shape(prediction)[0]) :
+		if prediction[i][1] > max_threshold :
+			final_answer.append(2)
+		else :
+			final_answer.append(1)
 
 	result = pd.DataFrame([])
 	result['Real'] = list(data['hit_height'])
-	result['Predict'] = prediction
-
+	result['Predict'] = final_answer
+	print("Real count : ",Counter(result['Real']))
+	print("Predict count : ",Counter(result['Predict']))
 	result.to_csv(xgb_outputname, index=None)
 
-	print("Accuracy: "+str(accuracy_score(data['hit_height'], prediction)))
-	print("Precision: "+str(precision_score(data['hit_height'], prediction, labels = label, average=None)))
-	print("Recall: "+str(recall_score(data['hit_height'], prediction, labels = label, average=None)))
+	print("Accuracy: "+str(accuracy_score(data['hit_height'], final_answer)))
+	print("Precision: "+str(precision_score(data['hit_height'], final_answer, labels = label, average=None)))
+	print("Recall: "+str(recall_score(data['hit_height'], final_answer, labels = label, average=None)))
+	print("===============================")
 
 	# plot result chart
-	plot_chart(set_now, "XGB", model, list(data['hit_height']), prediction, label)
-
+	plot_chart(set_now, "XGB", model, list(data['hit_height']), final_answer, label)
 
 def Run(set_now, filename, svm_option, svm_model_name, svm_outputname, xgboost_option, xgboost_model_name, xgboost_outputname):
 	x_predict = LoadData(filename)
@@ -111,8 +166,7 @@ def Run(set_now, filename, svm_option, svm_model_name, svm_outputname, xgboost_o
 		print("")
 		print("XGBoost predict set"+str(set_now)+" done!")
 
-
 def exec(predict_set):
 	for i in predict_set:
-		Run(i, '../data/set'+str(i)+'_with_skeleton.csv', False, 'SVM_skeleton.joblib.dat', 'SVM_set'+str(i)+'_skeleton_out.csv', True, 'XGB_skeleton.joblib.dat', 'XGB_set'+str(i)+'_skeleton_out.csv')
+		Run(i, '../data/set'+str(i)+'_with_skeleton.csv', False, 'SVM_skeleton.joblib.dat', 'SVM_set'+str(i)+'_skeleton_out.csv', True, 'XGB.joblib.dat', 'XGB_set'+str(i)+'_skeleton_out.csv')
 exec([2, 3])
